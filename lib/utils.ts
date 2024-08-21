194c9177b99fa4b5b8bf9a0e346d74395d41a9f3
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-21 15:04:30 +0900

    fixup! [feat]: 日付のフォーマットを変更するコンポーネントを作成した

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+//shadcn
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+//dayjs(日付の表示形式変換)
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
+//shadcn
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+//dayjs
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const formatDate = (date: string) => {
+    return dayjs.utc(date).tz("Asia/Tokyo").format("YYYY/MM/DD");
+};
