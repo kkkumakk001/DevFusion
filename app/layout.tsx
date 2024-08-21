73576155df81ce5b0f76723e09e64699dfd94f0a
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-21 15:06:48 +0900

    fixup! [style]: 背景色のコントラストを落とした

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ja">
-            <body>{children}</body>
+            <body className="bg-[var(--background)]">{children}</body>
         </html>
     );
 }
