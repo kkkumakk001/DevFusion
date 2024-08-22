dbc2a769d2508a07107775920b4d46ff37c827de
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-22 13:47:03 +0900

    [feat]: "→"付きのボタンコンポーネントを作成
    一覧ページへ遷移させたい時、単体で目立たせたい時にこっちを使う
    
    矢印アイコンがいらない、タグなどで複数個のボタンが設置される時は
    shadcnのbuttonコンポーネントを使う

---
+++ components/ButtonLink.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+type Props = {
+    href: string;
+    children: React.ReactNode;
+};
+
+export default function ButtonLink({ href, children }: Props) {
+    return (
+        //右寄せ
+        // <div className="flex justify-end">
+        <Button asChild size="lg">
+            <Link href={href}>
+                {children}
+                <img className="ml-2 size-4" src="/arrow-right.svg"></img>
+            </Link>
+        </Button>
+        // </div>
+    );
+}
