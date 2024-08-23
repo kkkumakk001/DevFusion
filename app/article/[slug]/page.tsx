f5212d40fe671c7c31014148810e6ac6036dcb97
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-23 10:46:52 +0900

    [perf]: microCMSでプレビュー画面を作成するための設定

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -1,14 +1,20 @@
 import ArticleDetail from "@/components/ArticleDetail";
 import { getArticleDetail } from "@/lib/microcms";
+import { notFound } from "next/navigation";
 
 type Props = {
     params: {
         slug: string;
     };
+    searchParams: {
+        dk?: string;
+    };
 };
 
-export default async function Page({ params }: Props) {
-    const data = await getArticleDetail(params.slug);
+export default async function Page({ params, searchParams }: Props) {
+    const data = await getArticleDetail(params.slug, {
+        draftKey: searchParams.dk,
+    }).catch(notFound);
 
     return (
         <main>
