f0e3fad9488edd200253fa3fdfe5b7c0b93877ce
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-23 10:17:49 +0900

    [feat]: ページネーションコンポーネントを作成

---
+++ components/Pagination.tsx
@@ -0,0 +1,38 @@
+import { NEWS_LIST_LIMIT } from "../constants";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+type Props = {
+    totalCount: number;
+    current?: number;
+    basePath?: string;
+};
+
+export default function Pagination({ totalCount, current = 1, basePath = "/article" }: Props) {
+    const pages = Array.from({ length: Math.ceil(totalCount / NEWS_LIST_LIMIT) }, (_, i) => i + 1);
+
+    return (
+        <nav>
+            <ul className="flex justify-center items-center p-6 mt-6">
+                {pages.map((p) => (
+                    <li key={p} className="mx-1">
+                        {current !== p ? (
+                            <Link
+                                href={`${basePath}/page/${p}`}
+                                className="flex justify-center items-center w-9 h-9 rounded-sm"
+                            >
+                                {p}
+                            </Link>
+                        ) : (
+                            <Button asChild size="sm">
+                                <span className="flex justify-center items-center w-9 h-9 rounded-sm">
+                                    {p}
+                                </span>
+                            </Button>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
