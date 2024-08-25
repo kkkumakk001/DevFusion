d3d2a5e52e689db2b9898240357b50c0173f2be7
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-25 18:13:52 +0900

    [refactor]: 二重にmainタグを設定してしまっていたので削除した

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <main>
-            <section className="p-12 mb-6 bg-slate-300">
-                <h2 className="text-center">最新記事</h2>
+        <>
+            <section className="p-12 mb-6 bg-slate-300 relative grid">
+                <Link href="/article" className="absolute inset-0"></Link>
+                <span className="place-items-center text-center">最新記事</span>
             </section>
-            <section className="p-12 mb-6 bg-slate-300">
-                <h2 className="text-center">コンポーネント集</h2>
+            <section className="p-12 mb-6 bg-slate-300 relative grid">
+                <Link href="/component" className="absolute inset-0"></Link>
+                <span className="place-items-center text-center">コンポーネント集</span>
             </section>
-            <section className="p-12 mb-6 bg-slate-300">
-                <h2 className="text-center">Next.js周りの技術</h2>
-            </section>
-        </main>
+        </>
     );
 }
