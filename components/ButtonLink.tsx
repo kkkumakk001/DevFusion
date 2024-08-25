d63b14f6169f54d435051fabe2bf22b002a7cda8
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-25 16:40:07 +0900

    [perf]: imgタグから<Image>コンポーネントへ変更

--- components/ButtonLink.tsx
+++ components/ButtonLink.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 type Props = {
     href: string;
@@ -11,7 +12,13 @@ export default function ButtonLink({ href, children }: Props) {
         <Button asChild size="lg">
             <Link href={href}>
                 {children}
-                <img className="ml-2 size-4" src="/arrow-right.svg"></img>
+                <Image
+                    className="ml-2"
+                    width={16}
+                    height={16}
+                    alt="アイコン"
+                    src="/arrow-right.svg"
+                ></Image>
             </Link>
         </Button>
     );
