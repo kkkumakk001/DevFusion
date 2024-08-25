2a13212ed099098e4ebbae5a36ed366b5379e568
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-25 16:35:32 +0900

    [add]: サーチボックスの読み込み

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,5 +1,6 @@
 import ArticleList from "@/components/ArticleList";
 import Pagination from "@/components/Pagination";
+import SearchField from "@/components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/constants";
 import { getArticleList } from "@/lib/microcms";
 
@@ -8,8 +9,8 @@ export default async function Articles() {
         limit: NEWS_LIST_LIMIT,
     });
     return (
-        <main>
-            <h2>記事一覧</h2>
+        <main className="w-[90%] pt-12 mx-auto">
+            <SearchField />
             <ArticleList article={article} />
             <Pagination totalCount={totalCount} />
         </main>
--- app/article/search/page.tsx
+++ app/article/search/page.tsx
@@ -1,7 +1,25 @@
-export default function Page() {
+import ArticleList from "@/components/ArticleList";
+import SearchField from "@/components/SearchField";
+import { NEWS_LIST_LIMIT } from "@/constants";
+import { getArticleList } from "@/lib/microcms";
+
+type Props = {
+    searchParams: {
+        q?: string;
+    };
+};
+
+export default async function Page({ searchParams }: Props) {
+    const { contents: article } = await getArticleList({
+        limit: NEWS_LIST_LIMIT,
+        q: searchParams.q,
+    });
+
     return (
         <main>
             <h2>特定のキーワードで絞り込んだ記事の一覧</h2>
+            <SearchField />
+            <ArticleList article={article} />
         </main>
     );
 }
