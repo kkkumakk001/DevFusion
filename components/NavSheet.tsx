8d773ad10f3ada110a2461196d6d9aee48596a53
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-25 16:31:12 +0900

    [feat]: ハンバーガーメニューを作成

---
+++ components/NavSheet.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "../components/ui/sheet";
+import Link from "next/link";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
+
+const navLink = [
+    { name: "Home", href: "" },
+    { name: "Article", href: "article" },
+    { name: "Component", href: "component" },
+    { name: "Contact", href: "contact" },
+];
+
+export default function NavSheet({ className }: { className?: string }) {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className={cn(className, "flex items-center justify-center")}>
+            <Sheet open={open} onOpenChange={setOpen}>
+                <SheetTrigger className="md:hidden">
+                    <HamburgerMenuIcon></HamburgerMenuIcon>
+                </SheetTrigger>
+                <SheetContent
+                    side={"top"}
+                    className="flex h-screen flex-col flex-wrap items-center justify-center"
+                >
+                    {navLink.map((nav, index) => {
+                        return (
+                            <Link
+                                key={nav.name}
+                                href={`/${nav.href}`}
+                                className="hover:underline animate-accordion-down relative"
+                                onClick={() => {
+                                    setOpen(false);
+                                }}
+                            >
+                                <span className="absolute block h-full bg-background animate-slide-to-right delay-100"></span>
+                                <span>{nav.name}</span>
+                            </Link>
+                        );
+                    })}
+                </SheetContent>
+            </Sheet>
+        </div>
+    );
+}
