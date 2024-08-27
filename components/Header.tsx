b54fde85eba39058aeb667a087edbc3f2fed295f
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-27 16:00:53 +0900

    [feat]: 検索ページへのリンク付きの虫眼鏡アイコンを追加、ヘッダー内の3つのアイコンの大きさが同じになるように修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,21 @@ import Link from "next/link";
 import React from "react";
 import { ModeToggleButton } from "./ModeToggleButton";
 import NavSheet from "./NavSheet";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
 
 const Header = () => {
     return (
-        <header className="mx-auto flex w-[90%] items-center justify-between py-2">
-            <h1 className="font-semibold">
+        <header className="mx-auto flex w-[95%] items-center justify-between py-4">
+            <h1 className="text-2xl">
                 <Link href="/">DevFusion</Link>
             </h1>
-            <div className="flex wrap gap-4">
+            <div className="flex wrap">
+                <Button variant="noDesign" size="icon">
+                    <Link href="/article/search">
+                        <MagnifyingGlassIcon className="h-[1.2rem] w-[1.2rem]" />
+                    </Link>
+                </Button>
                 <ModeToggleButton />
                 <NavSheet className="md:hidden" />
             </div>
