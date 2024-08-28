863ae7eca64605f757180ae8f24c2a855a4cc54e
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-28 17:15:53 +0900

    [style]: stickyに変更し、すりガラス調の背景にした

--- app/article/layout.tsx
+++ app/article/layout.tsx
@@ -9,12 +9,13 @@ export const reavalidate = 60;
 export default function NewsLayout({ children }: Props) {
     return (
         <>
-            <h2 className="flex items-center gap-4 text-xl font-bold text-center -mt-[14px] mb-12">
+            {/* <h2 className="flex items-center gap-4 text-xl font-bold text-center -mt-[14px] mb-12">
                 <span className="flex-1 h-px bg-primary/50"></span>
                 記事ページ
                 <span className="flex-1 h-px bg-primary/50"></span>
-            </h2>
-            <div className="flex gap-8 -mt-[14px]">
+            </h2> */}
+            {/* <div className="flex gap-8 -mt-[14px]"> */}
+            <div className="flex gap-8">
                 <div className="flex-1  p-8 rounded-3xl">{children}</div>
                 <aside className="hidden lg:block w-1/3 max-w-[400px]  p-8 rounded-3xl">
                     <SideNav />
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,18 +7,20 @@ import { Button } from "./ui/button";
 
 const Header = () => {
     return (
-        <header className="mx-auto flex w-[95%] items-center justify-between py-4">
-            <h1 className="text-2xl">
-                <Link href="/">DevFusion</Link>
-            </h1>
-            <div className="flex wrap">
-                <Button variant="noDesign" size="icon">
-                    <Link href="/article/search">
-                        <MagnifyingGlassIcon className="h-[1.2rem] w-[1.2rem]" />
-                    </Link>
-                </Button>
-                <ModeToggleButton />
-                <NavSheet className="md:hidden" />
+        <header className="sticky top-0 z-100 py-4 border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ">
+            <div className="mx-auto flex w-[95%] items-center justify-between">
+                <h1 className="text-xl">
+                    <Link href="/">DevFusion</Link>
+                </h1>
+                <div className="flex wrap">
+                    <Button variant="noDesign" size="icon">
+                        <Link href="/article/search">
+                            <MagnifyingGlassIcon className="h-[1.2rem] w-[1.2rem]" />
+                        </Link>
+                    </Button>
+                    <ModeToggleButton />
+                    <NavSheet className="md:hidden" />
+                </div>
             </div>
         </header>
     );
