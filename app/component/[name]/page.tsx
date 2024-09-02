c8566d5afcb45f0a80075f23499a5ac0a7d4af7c
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-09-02 14:59:01 +0900

    [style]: 余白の調節

--- app/component/[name]/page.tsx
+++ app/component/[name]/page.tsx
@@ -14,10 +14,10 @@ export default function Page({ params: { name } }: { params: { name: string } })
         notFound();
     }
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center max-w-3xl mx-auto">
             <div className="w-full">
-                <p className="mb-4 font-semibold text-lg">{component.title}</p>
-                <div className="min-h-[300px] h-auto p-4 mb-4 border border-border rounded-xl flex justify-center items-center">
+                <p className="mt-8 mb-4 font-semibold text-lg">{component.title}</p>
+                <div className="min-h-[300px] h-auto p-4 mb-6 border border-border rounded-xl flex justify-center items-center">
                     <span>{component.componentName}</span>
                 </div>
                 <span className="flex justify-end">
