e87591e4807814745bb485e95f3a7f72794c3222
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-09-02 14:27:56 +0900

    サイドナビシートの領域外クリックを設定
    shadcnのsheetコンポーネントで元から領域外クリックは設定されているが、パラレルルートに対応はしておらずルーティング操作も同時に行う必要があった。

--- app/@componentDetail/(.)component/[name]/ComponentDetail.tsx
+++ app/@componentDetail/(.)component/[name]/ComponentDetail.tsx
@@ -1,84 +1,41 @@
-// "use client";
-
-// import { type ElementRef, useEffect, useRef } from "react";
-// import { useRouter } from "next/navigation";
-// import { createPortal } from "react-dom";
-
-// export function Modal({ children }: { children: React.ReactNode }) {
-//     const router = useRouter();
-//     const dialogRef = useRef<ElementRef<"dialog">>(null);
-
-//     useEffect(() => {
-//         if (!dialogRef.current?.open) {
-//             dialogRef.current?.showModal();
-//         }
-//     }, []);
-
-//     function onDismiss() {
-//         router.back();
-//     }
-
-//     return createPortal(
-//         <div className="modal-backdrop">
-//             <dialog ref={dialogRef} className="modal" onClose={onDismiss}>
-//                 {children}
-//                 <button onClick={onDismiss} className="close-button" />
-//             </dialog>
-//         </div>,
-//         document.getElementById("modal-root")!
-//     );
-// }
-
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-// import Link from "next/link";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
-import { cn } from "@/lib/utils";
+import { Sheet, SheetContent, SheetDescription, SheetTitle } from "@/components/ui/sheet";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
 export default function ComponentDetail({
     children,
-    className,
+    title,
 }: {
     children: React.ReactNode;
-    className?: string;
+    title: string;
 }) {
     const router = useRouter();
-    const [open, setOpen] = useState(false);
-    const insideRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        setOpen(true);
-    }, []);
-
-    useEffect(() => {
-        const el = insideRef.current;
-
-        if (!el) return;
-
-        const hundleClickOutside = (e: MouseEvent) => {
-            if (!el?.contains(e.target as Node)) {
-                router.back();
-            }
-        };
-
-        document.addEventListener("click", hundleClickOutside);
-
-        return () => {
-            document.removeEventListener("click", hundleClickOutside);
-        };
-    }, [insideRef]);
+    const [isOpen, setIsOpen] = useState(true);
+    const outsideClick = () => {
+        setIsOpen(false);
+        router.back();
+    };
 
     return (
-        <div ref={insideRef} className={cn(className, "flex items-center justify-center")}>
-            <Sheet open={open} onOpenChange={setOpen}>
-                <SheetContent side="right" className="flex items-center justify-center">
-                    {children}
-                    <button onClick={router.back}>一覧に戻る</button>
-                </SheetContent>
-            </Sheet>
+        <div>
+            {isOpen && (
+                <div>
+                    <Sheet open={isOpen} onOpenChange={outsideClick}>
+                        <SheetDescription>
+                            <VisuallyHidden.Root>説明文です</VisuallyHidden.Root>
+                        </SheetDescription>
+                        <SheetTitle>
+                            <VisuallyHidden.Root>{title}</VisuallyHidden.Root>
+                        </SheetTitle>
+                        <SheetContent side="right" className="w-full pt-16">
+                            {children}
+                        </SheetContent>
+                    </Sheet>
+                </div>
+            )}
         </div>
     );
 }
