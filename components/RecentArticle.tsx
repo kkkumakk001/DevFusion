90c049b46256791a4e79aa0716778f32c2abab7c
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-09-20 04:11:38 +0900

    [style]: コードの整理

--- components/RecentArticle.tsx
+++ components/RecentArticle.tsx
@@ -7,6 +7,7 @@ import { TOP_NEWS_LIMIT } from "@/constants";
 export default async function RecentArticle() {
     const { contents: article } = await getArticleList({
         limit: TOP_NEWS_LIMIT,
+        orders: "createdAt",
     });
     return (
         <section className="bg-background p-8 lg:p-12 rounded-xl mb-12 shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.1)] dark:shadow-[0_0_12px_6px_rgba(255,255,255,0.05)]">
