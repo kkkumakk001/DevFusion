32715aa2d27a7eca607880a47cf77915e8721f7f
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-09-28 19:07:51 +0900

    [refactor]: Heroコンポーネントの切り分け、読み込み

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import RecentArticle from "@/components/RecentArticle";
 import RecommendRecipe from "@/components/RecommendRecipe";
 
 export default async function Home() {
     return (
         <>
+            <Hero />
             <RecentArticle />
             <RecommendRecipe />
         </>
