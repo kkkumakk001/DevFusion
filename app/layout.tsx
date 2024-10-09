8fb4bdb5971b3cc102b5868093f82d538258ad35
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-10-09 23:02:40 +0900

    [fixed]: ライトモード時、オープニングアニメーションの文字が背景色と同化している問題を修正した
    テキストカラーを指定していなかったので気づかなかった

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             <body className={`notoSansJP.className -z-10`}>
                 <ThemeProvider
                     attribute="class"
-                    defaultTheme="dark"
+                    defaultTheme="light"
                     enableSystem
                     disableTransitionOnChange
                 >
--- components/TypingAnimation.tsx
+++ components/TypingAnimation.tsx
@@ -12,38 +12,37 @@ const TypingAnimation = ({ children }: Props) => {
     const [isFirstVisit, setIsFirstVisit] = useState(true);
     const fullText = typeof children === "string" ? children : "";
 
-    // 初回訪問時だけにするか迷ってる
     useEffect(() => {
-        // const hasVisited = localStorage.getItem("hasVisitedTyping");
-        // if (hasVisited) {
-        //     setIsFirstVisit(false);
-        //     setText(fullText);
-        //     setIsCompleted(true);
-        // } else {
-        let index = 0;
+        const hasVisited = localStorage.getItem("hasVisitedTyping");
+        if (hasVisited) {
+            setIsFirstVisit(false);
+            setText(fullText);
+            setIsCompleted(true);
+        } else {
+            let index = 0;
 
-        const typing = () => {
-            if (index < fullText.length) {
-                setText(fullText.slice(0, index + 1));
-                index++;
-                return window.setTimeout(typing, 80);
-            } else {
-                setIsCompleted(true);
-                localStorage.setItem("hasVisitedTyping", "true");
-                return undefined;
-            }
-        };
+            const typing = () => {
+                if (index < fullText.length) {
+                    setText(fullText.slice(0, index + 1));
+                    index++;
+                    return window.setTimeout(typing, 80);
+                } else {
+                    setIsCompleted(true);
+                    localStorage.setItem("hasVisitedTyping", "true");
+                    return undefined;
+                }
+            };
 
-        const timerId = typing();
+            const timerId = typing();
 
-        return () => {
-            if (timerId) clearTimeout(timerId);
-        };
-        // }
+            return () => {
+                if (timerId) clearTimeout(timerId);
+            };
+        }
     }, [fullText]);
 
     return (
-        <div className="px-4 text-wrap font-mono whitespace-pre-wrap overflow-hidden p-8 lg:p-12 rounded-xl">
+        <div className="px-4 text-primary text-wrap font-mono whitespace-pre-wrap overflow-hidden p-8 lg:p-12 rounded-xl">
             <span>{text}</span>
             {!isCompleted && isFirstVisit && (
                 <span className="ml-1 animate-blink-dot scale-150">●</span>
