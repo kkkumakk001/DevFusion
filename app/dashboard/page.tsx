7dcb1abc5df972fb3ad5024e2ade60aebf73a417
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-10-20 23:27:12 +0900

    [feat]: ブックマークした記事の一覧を表示するページを作成

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,7 @@
-import BookmarkArticle from "@/components/BookMarkArticle";
-
 export default async function Page() {
     return (
-        <div className="flex-1 w-full flex flex-col gap-20 items-center pt-24">
-            <BookmarkArticle></BookmarkArticle>
+        <div>
+            <h2>ダッシュボードページ</h2>
         </div>
     );
 }
