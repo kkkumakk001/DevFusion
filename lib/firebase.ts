8a451dbfbac490467b21cc3bc6fdc3c183ab0da1
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-10-21 00:11:36 +0900

    [style]: コードの整形

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,22 +2,22 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STRAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MASSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-};
 // const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STRAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MASSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//     apiKey: process.env.FIREBASE_APIKEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STRAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MASSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID,
 // };
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STRAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MASSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 // const db = getFirestore(app);
